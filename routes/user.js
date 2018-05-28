1f22adecc905ce5b1a900375acb58aea35727608
Patrik Nilsson <pn222en@student.lnu.se>
2018-05-28 21:46:01 +0200

    add validation to login route

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,28 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const validateSignup = require('../validations/signup');
+const validateLogin = require('../validations/login');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
 
 router.post('/login', (req, res, next) => {
+	var user = new User();
+	console.log(req.body);
+	const { errors, isValid } = validateLogin(req.body);
+
+	// Check Validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
 	User.findOne({
-		username: req.body.user.username
+		username: req.body.username
 	})
 		.then(user => {
 			if (!user) {
 				return res.status(401).send('Wrong password or username');
 			}
-			if (user.validPassword(req.body.user.password)) {
+			if (user.validPassword(req.body.password)) {
 				return res.send(user.generateJWT());
 			} else {
 				return res.status(401).send('Wrong password or username');
