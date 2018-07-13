2120bb637bfcba9ab2de860bc5df672f41807e3e
Patrik Nilsson <pn222en@student.lnu.se>
2018-07-13 18:59:43 +0200

    add function to write imgs from unsplash to json file, can now seed database with fake data

---
+++ dummyImages.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const fs = require("fs");
+const { unsplash } = require("./config/apikeys");
+
+function getImages() {
+  let promiseArray = [];
+  let finalResults = [];
+  for (let i = 1; i < 45; i++) {
+    promiseArray.push(
+      axios.get("https://api.unsplash.com/photos", {
+        params: {
+          client_id: unsplash.client_id,
+          per_page: 30,
+          page: i
+        }
+      })
+    );
+  }
+
+  Promise.all(promiseArray)
+    .then(function(responses) {
+      let mergedResults = responses.map(response => {
+        return response.data;
+      });
+
+      mergedResults = [].concat.apply([], mergedResults);
+
+      finalResults = mergedResults
+        .filter(img => {
+          return img.width > img.height;
+        })
+        .map(test => {
+          return test.urls.regular;
+        });
+
+      fs.writeFile(
+        "dummyImages.json",
+        JSON.stringify(finalResults, null, 4),
+        "utf8",
+        () =>
+          console.log(finalResults.length + " imgs saved in dummyImages.json")
+      );
+    })
+    .catch(e => console.log(e));
+}
+
+module.exports = getImages();
---
+++ seed.js
@@ -0,0 +1,101 @@
+const faker = require("faker");
+const mongoose = require("mongoose");
+const dbURI = "mongodb://127.0.0.1/my_database";
+let dummyImages = require("./dummyImages.json");
+const User = require("./models/User");
+const Image = require("./models/Image");
+const Comment = require("./models/Comment");
+
+const users = [];
+const images = [];
+const comments = [];
+const imagesGroupedById = {};
+
+mongoose.connection.on("connected", function() {
+  mongoose.connection.db
+    .dropDatabase()
+    .then(() => init())
+    .then(data => {
+      console.log(
+        `Database cleared & seed completed \ninserted \n${
+          data[0].length
+        } users \n${data[1].length} images \n${data[2].length} comments`
+      );
+      mongoose.connection.close();
+    })
+    .catch(e => console.log(e));
+});
+
+mongoose.connect(dbURI);
+mongoose.Promise = global.Promise;
+
+function generateRandomImages(numbersOfImgsToGenerate = 1000) {
+  let image;
+  for (var i = 0; i < numbersOfImgsToGenerate; i++) {
+    image = new Image();
+
+    image.tags = faker.lorem
+      .words(Math.floor(Math.random() * 4) + 3)
+      .split(" ");
+    image.path = faker.helpers.randomize(dummyImages);
+    image.description = faker.lorem.sentence();
+    image.author = users[Math.floor(Math.random() * (users.length - 1))]._id;
+    images.push(image);
+    imagesGroupedById[image._id] = image;
+  }
+
+  return images;
+}
+
+function generateRandomUsers(numbersOfUserToGenerate = 100) {
+  let user;
+  for (var i = 0; i < numbersOfUserToGenerate; i++) {
+    user = new User();
+    user.username =
+      faker.name.firstName() + (Math.floor(Math.random() * 9) + 1);
+    user.email = faker.internet.email();
+    user.setPassword("123456");
+    users.push(user);
+  }
+
+  return users;
+}
+
+function generateRandomComments(numbersOfCommentsToGenerate = 5000) {
+  let comment;
+  let commentsGroupedByImageId = {};
+  let startDate;
+  for (var i = 0; i < numbersOfCommentsToGenerate; i++) {
+    startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 2);
+    comment = new Comment();
+    comment.text = faker.lorem.sentence();
+    comment.author = users[Math.floor(Math.random() * (users.length - 1))]._id;
+    comment.image = images[Math.floor(Math.random() * (images.length - 1))]._id;
+    comment.createdAt = faker.date.between(startDate, new Date());
+    comments.push(comment);
+  }
+
+  comments.forEach(comment => {
+    if (!commentsGroupedByImageId.hasOwnProperty(comment.image)) {
+      commentsGroupedByImageId[comment.image] = [];
+    }
+    commentsGroupedByImageId[comment.image].push(comment);
+  });
+
+  Object.keys(commentsGroupedByImageId).forEach(imageId => {
+    imagesGroupedById[imageId].comments = commentsGroupedByImageId[imageId];
+  });
+}
+
+function init() {
+  generateRandomUsers();
+  generateRandomImages();
+  generateRandomComments();
+
+  return Promise.all([
+    User.insertMany(users),
+    Image.insertMany(images),
+    Comment.insertMany(comments)
+  ]).then(result => result);
+}
